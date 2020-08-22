87ec6b01e7f776994383d8839c2a85e999606434
sbalci0 <samilbalci.us@gmail.com>
2020-08-22 16:55:13 -0400

    JavaTekrar 2. Commit, ilk 5. gun

---
+++ src/gun4/JavaWideningCasting.java
@@ -0,0 +1,21 @@
+package gun4;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class JavaWideningCasting {
+    // Widening (Genisletme) Casting (Otomatik) Kucuk Bir Cinsi Genis cinse cevirme
+    // byte -> short -> char -> int -> long -> float -> double
+
+    public static void main(String[] args) {
+        int number = 50;
+        double decimal = number;
+
+        System.out.println(number);
+        System.out.println(decimal);
+
+    }
+
+
+
+
+}
---
+++ src/gun5/PrimitiveAndReference.java
@@ -0,0 +1,16 @@
+package gun5;
+
+import java.util.Scanner;
+
+public class PrimitiveAndReference {
+
+    public static void main(String[] args) {
+
+        int a = 234; //primitive data
+        String isim = "Ahmet"; //Reference
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
